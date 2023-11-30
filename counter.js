a03bfd8e01dec9c8f657a9306db8ebed07f6a23c
We0l <ilyarbatuhan@gmail.com>
2023-11-30 23:57:36 +0300

    trying to fix js initiliaze error
    don't blame me

--- counter.js
+++ counter.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, set, ref, get,push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, set, ref, get, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-const appSettings = {
+const appSettings2 = {
     databaseURL: "https://epicneuralgfail-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
-const app2 = initializeApp(appSettings)
-const database = getDatabase(app2)
-const viewCountInDB = ref(database, "ViewCount")
+const app2 = initializeApp(appSettings2)
+const database2 = getDatabase(app2)
+const viewCountInDB = ref(database2, "ViewCount")
 
 const sayiEl = document.getElementById("sayi")
 
